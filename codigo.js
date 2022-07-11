460dec4ecb9123d646f77de5d286f4f476d14a9a
Alavro Ramirez <alvis99laj@gmail.com>
2022-07-10 21:24:02 -0500

    Primer version

---
+++ codigo.js
@@ -0,0 +1,9 @@
+//let es para definir variables locales, o dentro de un rango
+//var variables globales
+//const constantes, no cambia(da error si se trata de cambiar)
+/*var texto = "texto";
+const numero = 3;
+let boleano = true;
+alert(texto + ' ' + numero + ' ' + boleano);*/
+var nombre = prompt("Escribe tu nombre");
+alert("hola " + nombre);
